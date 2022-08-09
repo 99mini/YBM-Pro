6d9276457363b6d0120198a10989e2fa02325f4c
mini <itman390@gmail.com>
2022-08-09 14:33:31 +0900

    init commit & ch1. 1~5

---
+++ 1급 파이썬 나의 풀이/1차 1급 풀이/1_5.py
@@ -0,0 +1,51 @@
+#You may use import as below.
+import math
+'''
+n = 2 -> 2
+
+n = 3 -> 4, 4
+1   2   3
+8   9   4
+6   7   5
+
+n = 4 -> 6, 6, 2
+1   2   3   4
+12  13  14  5
+11  16  15  6
+10  9   8   7
+
+n = 5 -> 8, 8, 4, 4
+1   2   3   4   5
+16  17  18  19  6
+15  24  25  20  7
+14  23  22  21  8
+13  12  11  10  9
+
+n = 6 -> 10, 10, 6, 6, 2
+1   2   3   4   5   6
+20  21  22  23  24  7
+19  32  33  34  25  8
+18  31  36  35  26  9
+17  30  29  28  27  10
+16  15  14  13  12  11
+
+'''
+# TODO
+def solution(n):
+    # Write code here.
+    answer = 0
+    return answer
+
+
+#The following is code to output testcase.
+n1 = 3
+ret1 = solution(n1)
+
+#Press Run button to receive output. 
+print("Solution: return value of the function is", ret1, ".")
+    
+n2 = 2
+ret2 = solution(n2)
+
+#Press Run button to receive output. 
+print("Solution: return value of the function is", ret2, ".")
